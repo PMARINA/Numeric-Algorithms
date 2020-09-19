83e1ae22e4adef295a61570f638c38c6eb915b3b
PMARINA <bluerat33@gmail.com>
2020-09-18 23:23:18 -0400

    Implemented GCD using Euclid's Algorithm

--- gcd.cc
+++ gcd.cc
@@ -1,5 +1,9 @@
 #include "main.h"
-
+//Euclid's method
 uint64_t gcd(uint64_t a, uint64_t b){
-	return 0;
+	if(a<b)return gcd(b,a);
+	if(a == b) return b;
+	uint64_t mod = a%b;
+	if(mod == 0) return b;
+	return gcd(b,mod);
 }
